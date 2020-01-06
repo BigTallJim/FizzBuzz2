7a58f7c5c0431cad9a97892c2e8a70721f7be7e5
James Holton <james.holton@yahoo.co.uk>
2020-01-06 11:12:31 +0000

    first commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,11 @@
+describe('Testing FizzBuzz', function(){
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
+
+  // it("It returns 1 when passed 1", function(){
+  //   expect(fizzBuzz.play(1)).toEqual(1);
+  // });
+
+});
