231f66c532bbddbdf7129647653f11fa07fe8c78
mariacuffaro <maria.cuffaro@johnlewis.co.uk>
2020-01-06 11:21:55 +0000

    First test passed, second failing test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -4,8 +4,10 @@ describe('Testing FizzBuzz', function(){
     fizzBuzz = new FizzBuzz();
   });
 
-  // it("It returns 1 when passed 1", function(){
-  //   expect(fizzBuzz.play(1)).toEqual(1);
-  // });
-
+  it("It returns 1 when passed 1", function(){
+    expect(fizzBuzz.play(1)).toEqual(1);
+  });
+  it("It returns 2 when passed 2", function(){
+    expect(fizzBuzz.play(2)).toEqual(2);
+  });
 });
